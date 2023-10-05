615b091113386bdaa59f4731c066ca75612327df
Vinyas Kulal <98479341+Vinyas-13@users.noreply.github.com>
2023-10-05 10:44:54 +0530

    Time: 4 ms (93.15%), Space: 16.1 MB (28.20%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,63 +1,47 @@
 class Solution {
 public:
-       vector<int> majorityElement(vector<int>& nums) {
-       vector<int>ans;
-       int cnt1=0;
-       int cnt2=0;
-       int el1=INT_MIN;
-       int el2=INT_MIN;
-
-       for(int i=0;i<nums.size();i++)
-       {
-           if(cnt1==0 && nums[i]!=el2)
+    //Moore's Voting Algorithm
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1=0;
+        int cnt2=0;
+        int el1=0;
+        int el2=0;
+        int n=nums.size();
+        vector<int>majElements;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(cnt1==0 && nums[i]!=el2)
             {
-               el1=nums[i];
-               cnt1=1;   
-            } 
+                el1=nums[i];
+                cnt1=1;
+            }
             else if(cnt2==0 && nums[i]!=el1)
             {
                 el2=nums[i];
                 cnt2=1;
             }
-            else if(nums[i]==el1)
-            {
-                cnt1++;
-            }
-            else if(nums[i]==el2)
-            {
-                cnt2++;
-            }
+            else if(nums[i]==el1)cnt1++;
+            else if(nums[i]==el2)cnt2++;
             else
             {
                 cnt1--;
                 cnt2--;
             }
-       } 
-
-       cnt1=0;
-       cnt2=0;
-       int mini=nums.size()/3+1;
-       for(int i=0;i<nums.size();i++)
-       {
-           if(nums[i]==el1)
-           {
-               cnt1++;
-           }
-           if(nums[i]==el2)
-           {
-               cnt2++;
-           }
-
-       }
-       if(cnt1>=mini)
-       {
-           ans.push_back(el1);
-       }
-        if(cnt2>=mini)
-       {
-           ans.push_back(el2);
-       }
-
-       return ans;
+        }
+        
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==el1)cnt1++;
+            else if(nums[i]==el2)cnt2++;
+        }
+        
+        int minMaj=n/3+1;
+        if(cnt1>=minMaj)majElements.push_back(el1);
+        if(cnt2>=minMaj)majElements.push_back(el2);
+        return majElements; 
     }
+    
 };
